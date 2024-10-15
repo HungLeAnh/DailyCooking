b4df4663d412415b772ecdac7092377603dba7a7
HungLA <leanhung.bhn@gmail.com>
2024-10-15 18:57:39 +0700

    player input action

---
+++ DailyCooking/Assets/Scripts/Player/MonoSingleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject gameObject = new GameObject(nameof(T));
+                    _instance = gameObject.AddComponent<T>();
+                }
+            }
+            return _instance;
+
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this as T;
+        else
+        {
+            if(_instance!= this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
