46be164b3c5f1cdbbebdff477ba6eb3c981ea395
HungLA <leanhung.bhn@gmail.com>
2024-11-20 02:12:13 +0700

    new UI

---
+++ DailyCooking/Assets/Scripts/Counters/IHasOptionalSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public interface IHasOptionalSO
+{
+    public void SetOptionKitchenObjectSO(int index);
+    public virtual List<KitchenObjectSO> GetListKitchenObjectList(KitchenObjectSO kitchenObjectSO)
+    {
+        return null;
+    }
+}
---
+++ DailyCooking/Assets/Scripts/UI/BurnWarningUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class BurnWarningUI : MonoBehaviour
+{
+    private IHasProgress _IHasProgressGameObject;
+
+    private void Start()
+    {
+        _IHasProgressGameObject = GetComponentInParent<IHasProgress>();
+        if( _IHasProgressGameObject != null)
+        {
+            _IHasProgressGameObject.OnProgressChanged += IHasProgressGameObject_OnProgressChanged;
+        }
+        Hide();
+    }
+
+    private void IHasProgressGameObject_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
+    {
+        float burnShowProgressAmount = .5f;
+
+        bool show = _IHasProgressGameObject.IsDone() && e.progressNormalized >= burnShowProgressAmount;
+
+        if (show)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+
+    }
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+
+    }
+
+}
